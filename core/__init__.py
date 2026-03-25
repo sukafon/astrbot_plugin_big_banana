67960e9e2bccf4a4cbb733a7cb829599ef338ee6
sukafon <dusk_dn@outlook.com>
2026-03-25 18:01:01 +0800

    fix: 未清理模块导入导致的错误

--- core/__init__.py
+++ core/__init__.py
@@ -3,7 +3,6 @@ from .downloader import Downloader
 from .gemini import GeminiProvider
 from .http_manager import HttpManager
 from .openai_chat import OpenAIChatProvider
-from .vertex_ai import VertexAIProvider
 from .vertex_ai_anonymous import VertexAIAnonymousProvider
 
 __all__ = [
@@ -12,6 +11,5 @@ __all__ = [
     "BaseProvider",
     "GeminiProvider",
     "OpenAIChatProvider",
-    "VertexAIProvider",
     "VertexAIAnonymousProvider",
 ]
